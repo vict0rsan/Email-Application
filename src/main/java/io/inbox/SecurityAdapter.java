4ec4d8aaaecd5701b0f38fa161a856713ba66021
vict0rsan <vicloligye@hotmail.com>
2022-07-02 19:57:25 +0200

    Setting up project

--- src/main/java/io/inbox/SecurityAdapter.java
+++ src/main/java/io/inbox/SecurityAdapter.java
@@ -1,4 +1,4 @@
-package io.javabrains.inbox;
+package io.inbox;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -12,7 +12,6 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// @formatter:off
 		http
 			.authorizeRequests(a -> a
 				.antMatchers("/", "/error").permitAll()
@@ -28,7 +27,6 @@ public class SecurityAdapter extends WebSecurityConfigurerAdapter {
 				.logoutSuccessUrl("/").permitAll()
 			)
 			.oauth2Login();
-		// @formatter:on
     }
     
 }
--- src/main/java/io/inbox/SpringGitHubLoginApplication.java
+++ src/main/java/io/inbox/SpringGitHubLoginApplication.java
@@ -1,11 +1,9 @@
-package io.javabrains.inbox;
+package io.inbox;
 
 import java.nio.file.Path;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,12 +22,6 @@ public class SpringGitHubLoginApplication {
 		System.out.println(principal);
 		return principal.getAttribute("name");
 	}
-
-	@Bean
-    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-        Path bundle = astraProperties.getSecureConnectBundle().toPath();
-        return builder -> builder.withCloudSecureConnectBundle(bundle);
-    }
 	
 
 }
