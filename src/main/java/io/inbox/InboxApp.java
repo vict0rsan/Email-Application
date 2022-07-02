1f3f7d4882037f58c7eaec0169f478d6821e5bdf
vict0rsan <vicloligye@hotmail.com>
2022-07-02 20:24:31 +0200

    Working login

--- src/main/java/io/inbox/InboxApp.java
+++ src/main/java/io/inbox/InboxApp.java
@@ -1,7 +1,5 @@
 package io.inbox;
 
-import java.nio.file.Path;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringGitHubLoginApplication {
+public class InboxApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGitHubLoginApplication.class, args);
+		SpringApplication.run(InboxApp.class, args);
 	}
 
 	@RequestMapping("/user")
