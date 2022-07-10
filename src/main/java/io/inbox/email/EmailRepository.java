83798c5dfddb1be30e5e8e524932b11555be36b2
vict0rsan <vicloligye@hotmail.com>
2022-07-10 16:31:26 +0200

    application.yml formatted

--- src/main/java/io/inbox/email/EmailRepository.java
+++ src/main/java/io/inbox/email/EmailRepository.java
@@ -3,7 +3,9 @@ package io.inbox.email;
 import java.util.UUID;
 
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmailRepository extends CassandraRepository<Email, UUID>{
     
 }
