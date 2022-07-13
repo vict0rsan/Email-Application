12ba5e8a67563dfe0b65c58e503b1f0989436014
vict0rsan <vicloligye@hotmail.com>
2022-07-13 22:00:34 +0200

    Counters working fine + style for unread messages

--- src/main/java/io/inbox/controllers/EmailController.java
+++ src/main/java/io/inbox/controllers/EmailController.java
@@ -13,12 +13,17 @@ import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import io.inbox.emaiList.EmailListItem;
+import io.inbox.emaiList.EmailListItemKey;
+import io.inbox.emaiList.EmailListItemRepository;
 import io.inbox.email.Email;
 import io.inbox.email.EmailRepository;
 import io.inbox.folders.Folder;
 import io.inbox.folders.FolderRepository;
 import io.inbox.folders.FolderService;
+import io.inbox.folders.UnreadEmailStatsRepository;
 
 @Controller
 public class EmailController {
@@ -32,11 +37,20 @@ public class EmailController {
     @Autowired
     private EmailRepository emailRepository;
 
+    @Autowired
+    private EmailListItemRepository emailListItemRepository;
+
+    @Autowired
+    private UnreadEmailStatsRepository unreadEmailStatsRepository;
+
 
     @GetMapping(value = "/emails/{id}")    
-    public String emailView(@PathVariable UUID id,
+    public String emailView(
+        @RequestParam String folder,
+        @PathVariable UUID id,
         @AuthenticationPrincipal OAuth2User principal,
-        Model model){
+        Model model
+        ){
         
         if(principal == null || !StringUtils.hasText(principal.getAttribute("login")))
             return "index";
@@ -46,8 +60,6 @@ public class EmailController {
         model.addAttribute("userFolders", userFolders);
         List<Folder> defaultFolders = folderService.fetchDefaultFolders(userId);
         model.addAttribute("defaultFolders", defaultFolders);
-        Map<String, Integer> unreadEmailStats = folderService.mapCountToLabels(userId);
-        model.addAttribute("stats", unreadEmailStats);
 
         Optional<Email> optionalEmail = emailRepository.findById(id);
 
@@ -56,16 +68,29 @@ public class EmailController {
         }
 
         Email email = optionalEmail.get();
-
         String destinations = String.join(", ", email.getDestination());
-
         model.addAttribute("email", email);
         model.addAttribute("destinations", destinations);
 
+        EmailListItemKey key = new EmailListItemKey();
+        key.setId(userId);
+        key.setLabel(folder);
+        key.setTimeUUID(email.getId());
+        Optional<EmailListItem> optionalEmailListItem = emailListItemRepository.findById(key);
 
-            
+        if(optionalEmailListItem.isPresent()){
+            EmailListItem emailListItem = optionalEmailListItem.get();
+            if(emailListItem.getIsRead() == false){
+                emailListItem.setRead(true);
+                emailListItemRepository.save(emailListItem);
+                unreadEmailStatsRepository.decrementUnreadCounter(userId, folder);
+            }
+        }
+
+        Map<String, Integer> unreadEmailStats = folderService.mapCountToLabels(userId);
+        model.addAttribute("stats", unreadEmailStats);
+
         return "email-page";
-        
     }
     
 }
